8c14686818d5111a4e248a4794582e70ae265aee
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-04 23:39:19 +0300

    content to fit entire container

--- src/pages/components/HomePage/index.jsx
+++ src/pages/components/HomePage/index.jsx
@@ -3,22 +3,22 @@ import ImageSlider from '../ImageSlider';
 import Footer from '../Footer';
 
 const HomePage = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <div className="flex-grow w-full">
-        <div>
-          <ImageSlider />
+    return (
+        <div className="flex flex-col flex-grow">
+
+            <div>
+                <ImageSlider />
+            </div>
+            {/* Home page content goes here */}
+            <div className="flex flex-col items-center justify-center flex-grow">
+                <h1 className="text-3xl font-bold mb-4" >Welcome to Bliss n Blooms Events</h1>
+                <p className="text-lg">We are a premier event planning company dedicated to creating unforgettable experiences.</p>
+                {/* Add more content as needed */}
+            </div>
+
+            <Footer />
         </div>
-        {/* Home page content goes here */}
-        <div className="w-full">
-          <h1>Welcome to Bliss n Blooms Events</h1>
-          <p>We are a premier event planning company dedicated to creating unforgettable experiences.</p>
-          {/* Add more content as needed */}
-        </div>
-      </div>
-      <Footer />
-    </div>
-  );
+    );
 };
 
 export default HomePage;
