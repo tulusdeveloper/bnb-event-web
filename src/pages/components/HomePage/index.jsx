53f7aaad43d374981652b2bcf09c1e1a31e41550
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-05 00:04:00 +0300

    footer bottom positioning

--- src/pages/components/HomePage/index.jsx
+++ src/pages/components/HomePage/index.jsx
@@ -3,22 +3,20 @@ import ImageSlider from '../ImageSlider';
 import Footer from '../Footer';
 
 const HomePage = () => {
-    return (
-        <div className="flex flex-col flex-grow">
-
-            <div>
-                <ImageSlider />
-            </div>
-            {/* Home page content goes here */}
-            <div className="flex flex-col items-center justify-center flex-grow">
-                <h1 className="text-3xl font-bold mb-4" >Welcome to Bliss n Blooms Events</h1>
-                <p className="text-lg">We are a premier event planning company dedicated to creating unforgettable experiences.</p>
-                {/* Add more content as needed */}
-            </div>
-
-            <Footer />
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow">
+        <ImageSlider />
+        {/* Home page content goes here */}
+        <div className="flex flex-col items-center justify-center flex-grow">
+          <h1 className="text-3xl font-bold mb-4">Welcome to Bliss n Blooms Events</h1>
+          <p className="text-lg">We are a premier event planning company dedicated to creating unforgettable experiences.</p>
+          {/* Add more content as needed */}
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default HomePage;
