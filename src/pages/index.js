eff942453ba2ed8e4b4064ef2a93b1551e912311
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-04 13:43:16 +0300

    added navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div className={`flex min-h-screen ${inter.className}`}>
       {/* Left Navigation */}
-      <nav className="bg-white shadow-md p-6 w-64">
+      <nav className="bg-white shadow-md p-10 w-80 flex flex-col justify-between">
         {/* Logo */}
-        <div className="p-6">
+        <div className="p-10">
           <Link href="/">
             <Image
               src="/blissnbloomslogo.png"
@@ -23,44 +23,63 @@ export default function Home() {
         </div>
 
         {/* Navigation Links */}
-        <ul className="space-y-4 flex flex-col items-center">
-          <li>
-            <a
-              href="#"
-              className="flex items-center text-gray-700 hover:text-gray-900"
-            >
-              {/* You can add icons here */}
-              <span>Home</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center text-gray-700 hover:text-gray-900"
-            >
-              {/* You can add icons here */}
-              <span>About</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center text-gray-700 hover:text-gray-900"
-            >
-              {/* You can add icons here */}
-              <span>Services</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="flex items-center text-gray-700 hover:text-gray-900"
-            >
-              {/* You can add icons here */}
-              <span>Contact</span>
-            </a>
-          </li>
-        </ul>
+        <div>
+          <ul className="space-y-6 flex flex-col items-center">
+            <li>
+              <Link
+                href="#"
+                className="flex items-center text-gray-700 hover:text-gray-900"
+
+              >
+                {/* You can add icons here */}
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="flex items-center text-gray-700 hover:text-gray-900"
+              >
+                {/* You can add icons here */}
+                <span>About</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="flex items-center text-gray-700 hover:text-gray-900"
+              >
+                {/* You can add icons here */}
+                <span>Services</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                className="flex items-center text-gray-700 hover:text-gray-900"
+              >
+                {/* You can add icons here */}
+                <span>Contact</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        {/* Social Links */}
+        <div className="flex flex-col items-center text-gray-600">
+          <Link href="mailto:blissnbloomsevents@gmail.com" className="hover:text-gray-900">
+            blissnbloomsevents@gmail.com
+          </Link>
+          <Link href="mailto:hello@blissnbloomsevents.co.ke" className="hover:text-gray-900">
+            hello@blissnbloomsevents.co.ke
+          </Link>
+          <Link href="tel:+254715223428" className="hover:text-gray-900">
+            +254 715 223428
+          </Link>
+          <Link href="tel:+254710391049" className="hover:text-gray-900">
+            +254 710 391 049
+          </Link>
+        </div>
       </nav>
 
       {/* Main Content */}
