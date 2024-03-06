2db8304f9c88fe620c113fea099325ccd4564d1e
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-06 17:12:11 +0300

    changes

--- src/pages/components/AboutPage/index.jsx
+++ src/pages/components/AboutPage/index.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
-
+import Image from 'next/image';
 
 const AboutPage = () => {
-return (
-  <div className="flex flex-col min-h-screen ml-80">
-    <div className="flex-grow">
-     <Header />
-      {/* Home page content goes here */}
-      <div className="flex flex-col items-center justify-center flex-grow">
-        <h1 className="text-3xl font-bold mb-4">About Bliss n Blooms Events</h1>
-        <p className="text-lg">Insert information about your company here.</p>
-        {/* Add more content as needed */}
+  return (
+    <div className="flex flex-col min-h-screen ml-80">
+      <Header />
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <div className="max-w-4xl px-6">
+          <h1 className="text-4xl font-serif text-gray-800 mb-6">About Bliss & Blooms Events</h1>
+          <div className="w-16 h-1 bg-pink-500 mb-4"></div>
+          <p className="text-lg text-gray-600 leading-relaxed">
+            At Bliss & Blooms we committed to providing our clients with the highest level of service and attention to detail. We believe that every event should be a celebration, we are dedicated to making your event the best it can be. Bliss & Blooms was founded in 2022 by Allan Rop and Nicole Rop, a family which shared a passion for events and design. They started the company with the goal of creating beautiful and memorable events that would reflect our clients unique style and vision. In the years since, Bliss & Blooms has grown to become one of the leading event planning and design companies in Eldoret. They have planned and executed events of all sizes, from intimate weddings to large corporate events. They are known for their attention to detail, their creativity, and their commitment to making their clients events unforgettable. Bliss & Blooms is a company that is constantly growing and evolving. They are committed to providing their clients with the best possible experience, and they are always looking for new ways to create beautiful and memorable events.
+          </p>
+          <h2 className="text-2xl font-semibold text-gray-800 mt-8 mb-4">Vision & Mission</h2>
+          <div className="w-16 h-1 bg-pink-500 mb-4"></div>
+          <div className="mb-6">
+            <h3 className="text-xl font-semibold text-gray-800 mb-2">Vision</h3>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              The vision of Bliss & Blooms is to be the leading event planning and design company in Kenya. They aspire to create beautiful and memorable events that reflect their clients unique style and vision. They also aim to provide their clients with the highest level of service and attention to detail.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-xl font-semibold text-gray-800 mb-2">Mission</h3>
+            <p className="text-lg text-gray-600 leading-relaxed">
+              The mission of Bliss & Blooms is to create beautiful and memorable events that reflect their clients unique style and vision. They believe that every event should be a celebration, and they are dedicated to making their clients events the best they can be. We are committed to providing their clients with the best possible experience, and they are always looking for new ways to make their events even more special.
+            </p>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
 };
 
 export default AboutPage;
--- src/pages/components/OurWork/index.jsx
+++ src/pages/components/OurWork/index.jsx
@@ -1,23 +1,78 @@
 import React from 'react';
 import Footer from '../Footer';
 import Header from '../Header';
+import Image from 'next/image';
 
+
+
+  const ServiceCard = ({ imageSrc, alt, title, description }) => {
+  return (
+    <div className="bg-gray-200 shadow-md rounded-lg p-4 flex flex-col items-center transition-transform duration-300 hover:scale-105">
+      <Image
+        src={imageSrc}
+        alt={alt}
+        width={800}
+        height={600}
+        className="w-full h-48 object-cover rounded-lg mb-4"
+      />
+      <h3 className="text-xl font-bold mb-2 text-pink-600">{title}</h3>
+      <p className="text-md text-center px-4">{description}</p>
+    </div>
+  );
+};
 
 const OurWork = () => {
-return (
-  <div className="flex flex-col min-h-screen ml-80">
-    <div className="flex-grow">
-     <Header/>
-      {/* Home page content goes here */}
-      <div className="flex flex-col items-center justify-center flex-grow">
-        <h1 className="text-3xl font-bold mb-4">Our Work</h1>
-        <p className="text-lg">Check out our previous work.</p>
-        {/* Add more content as needed */}
+  const services = [
+    {
+      imageSrc: "/services/0Q0A6863.jpg",
+      alt: "Luxury Weddings",
+      title: "Luxury Weddings",
+      description: "We create luxurious and unforgettable wedding experiences for our clients."
+    },
+    {
+      imageSrc: "/services/BPK-101.jpg",
+      alt: "Destination Weddings",
+      title: "Destination Weddings",
+      description: "Plan your dream destination wedding with our expert team. We handle all the details, so you can relax and enjoy your special day."
+    },
+    {
+      imageSrc: "/services/ROP08221.jpg",
+      alt: "Small Celebrations",
+      title: "Small Celebrations",
+      description: "We also specialize in intimate gatherings and small celebrations."
+    },
+    {
+      imageSrc: "/services/0Q0A6885.jpg",
+      alt: "Event Planning and Coordination",
+      title: "Event Planning and Coordination",
+      description: "Our team of experienced event planners will work with you to create a custom event plan tailored to your budget and timeline."
+    },
+    {
+      imageSrc: "/services/BPK-102.jpg",
+      alt: "Corporate Events",
+      title: "Corporate Events",
+      description: "We offer comprehensive corporate event planning services, from conferences and seminars to product launches and team-building events."
+    }
+  ];
+
+  return (
+    <div className="flex flex-col min-h-screen ml-80">
+      <Header />
+      <div className="flex-grow flex flex-col justify-center items-center py-16">
+        <h1 className="text-4xl font-serif text-gray-800 mb-6">Our Work</h1>
+        <div className="w-16 h-1 bg-pink-500 mb-4"></div>
+        <p className="text-lg text-gray-600 max-w-3xl text-center leading-relaxed">
+          At Bliss & Blooms, we curate exceptional events that leave a lasting impression. From intimate gatherings to grand celebrations, our team of dedicated professionals brings your vision to life with meticulous attention to detail and a touch of elegance. Indulge in unforgettable experiences tailored to your unique style and preferences.
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
+          {services.map((service, index) => (
+            <ServiceCard key={index} {...service} />
+          ))}
+        </div>
       </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
 };
 
 export default OurWork;
