e0080f8a5877988e52f8ee20c00df7f2275f997c
tulusdeveloper <tulusdeveloper@gmail.com>
2024-03-13 10:16:26 +0300

    dark mode application

--- src/pages/components/OurWork/index.jsx
+++ src/pages/components/OurWork/index.jsx
@@ -8,7 +8,7 @@ import Image from 'next/image';
   const ServiceCard = ({ imageSrc, alt, title, description }) => {
   return (
     // <div className="bg-gray-200 shadow-md rounded-lg p-4 flex flex-col items-center transition-transform duration-300 hover:scale-105">
-    <div className="bg-gray-800 shadow-md rounded-lg p-4 flex flex-col items-center transition-transform duration-300 hover:scale-105">
+    <div className="bg-gray-200 dark:bg-gray-800 shadow-md rounded-lg p-4 flex flex-col items-center transition-transform duration-300 hover:scale-105">
 
       <Image
         src={imageSrc}
@@ -17,8 +17,8 @@ import Image from 'next/image';
         height={600}
         className="w-full h-48 object-cover rounded-lg mb-4"
       />
-      <h3 className="text-xl font-bold mb-2 text-pink-600">{title}</h3>
-      <p className="text-md text-center px-4">{description}</p>
+      <h3 className="text-xl font-bold mb-2 text-pink-600 dark:text-pink-400">{title}</h3>
+      <p className="text-md text-center px-4 text-gray-600 dark:text-gray-300">{description}</p>
     </div>
   );
 };
@@ -61,9 +61,9 @@ const OurWork = () => {
     <div className="flex flex-col min-h-screen ml-80">
       <Header />
       <div className="flex-grow flex flex-col justify-center items-center py-16">
-        <h1 className="text-4xl font-serif text-gray-800 mb-6">Our Work</h1>
-        <div className="w-16 h-1 bg-pink-500 mb-4"></div>
-        <p className="text-lg text-gray-600 max-w-3xl text-center leading-relaxed">
+        <h1 className="text-4xl font-serif text-gray-800 dark:text-white mb-6">Our Work</h1>
+        <div className="w-16 h-1 bg-pink-500 dark:bg-pink-400 mb-4"></div>
+        <p className="text-lg text-gray-600 dark:text-gray-300 max-w-3xl text-center leading-relaxed">
           At Bliss & Blooms, we curate exceptional events that leave a lasting impression. From intimate gatherings to grand celebrations, our team of dedicated professionals brings your vision to life with meticulous attention to detail and a touch of elegance. Indulge in unforgettable experiences tailored to your unique style and preferences.
         </p>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-8">
